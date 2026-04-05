196f4a2041800f01e9699ac3fb7b41afe756d9ae
2301_76474443 <2301_76474443@noreply.gitcode.com>
2026-04-06 01:53:35 +0800

    [feature] app icon

--- src/ui/app/main.cpp
+++ src/ui/app/main.cpp
@@ -1,10 +1,12 @@
 #include "Gg.h"
 
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(QStringLiteral(":/Gg/app_icon.png")));
     Gg window;
     window.show();
     return app.exec();
